3c6766ff32432e7d947deaa86fb6be217b779d6a
Gizem ALTINCI <gizemaltnc@outlook.com>
2025-11-24 22:34:42 +0300

    ...

--- ApiNight.WebApi/obj/Debug/net6.0/ApiNight.WebApi.AssemblyInfo.cs
+++ ApiNight.WebApi/obj/Debug/net6.0/ApiNight.WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiNight.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d369723f891634db3e2f2d67d3c676baa43e7a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1534dc43afa07bca580c2e6598d897347dcabfe8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiNight.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiNight.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
